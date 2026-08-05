8d1747e282a69081f546386a52f7ffef1e4bae14
Daniel Muritiba <danielsmuritiba@gmail.com>
2026-08-05 19:33:42 -0300

    Inicio

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <header>
+        <img src="/logo.png" alt="" />
+        <section>
+          <img src="/banner.png" alt="" />
+        </section>
+      </header>
+    </main>
+  );
+}
+
+export default App;
