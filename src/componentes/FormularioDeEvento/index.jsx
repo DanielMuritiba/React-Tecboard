69b7361beac05f648d4744d03296fb2a5a5d513b
Daniel Muritiba <danielsmuritiba@gmail.com>
2026-08-14 18:52:02 -0300

    estilização inicial

--- src/componentes/FormularioDeEvento/index.jsx
+++ src/componentes/FormularioDeEvento/index.jsx
@@ -8,10 +8,16 @@ export function FormularioDeEvento() {
   return (
     <form className="form-evento">
       <TituloFormulario>Preencha para criar um evento:</TituloFormulario>
-      <CampoDeFormulario>
-        <Label htmlFor="nome">Qual o nome do evento?</Label>
-        <CampoDeEntrada type="text" id="nome" placeholder="Summer dev hits" />
-      </CampoDeFormulario>
+      <div className="campos">
+        <CampoDeFormulario>
+          <Label htmlFor="nome">Qual o nome do evento?</Label>
+          <CampoDeEntrada type="text" id="nome" placeholder="Summer dev hits" />
+        </CampoDeFormulario>
+        <CampoDeFormulario>
+          <Label htmlFor="dataEvento">Data do evento</Label>
+          <CampoDeEntrada type="date" id="dataEvento" />
+        </CampoDeFormulario>
+      </div>
     </form>
   );
 }
