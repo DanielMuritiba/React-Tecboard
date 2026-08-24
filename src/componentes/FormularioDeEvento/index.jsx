bc75fa8e9600fe795b252beac0249ef1824d26de
Daniel Muritiba <danielsmuritiba@gmail.com>
2026-08-24 19:12:57 -0300

    Submit

--- src/componentes/FormularioDeEvento/index.jsx
+++ src/componentes/FormularioDeEvento/index.jsx
@@ -7,21 +7,43 @@ import { ListaSuspensa } from "../ListaSuspensa";
 import { Botao } from "../Botao";
 
 export function FormularioDeEvento({ temas }) {
+  function aoFormSubmetido(formData) {
+    console.log(formData);
+    const evento = {
+      capa: formData.get("capa"),
+      tema: temas.find(function (item) {
+        return item.id == formData.get("temaEvento");
+      }),
+      data: new Date(formData.get("dataEvento")),
+      titulo: formData.get("nomeEvento"),
+    };
+    console.log(evento);
+  }
   return (
-    <form className="form-evento">
+    <form className="form-evento" action={aoFormSubmetido}>
       <TituloFormulario>Preencha para criar um evento:</TituloFormulario>
       <div className="campos">
         <CampoDeFormulario>
-          <Label htmlFor="nome">Qual o nome do evento?</Label>
-          <CampoDeEntrada type="text" id="nome" placeholder="Summer dev hits" />
+          <Label htmlFor="nomeEvento">Qual o nome do evento?</Label>
+          <CampoDeEntrada
+            type="text"
+            name="nomeEvento"
+            id="nomeEvento"
+            placeholder="Summer dev hits"
+          />
         </CampoDeFormulario>
         <CampoDeFormulario>
           <Label htmlFor="capa">Qual o endereço da imagem de capa?</Label>
-          <CampoDeEntrada type="text" id="capa" placeholder="http://..." />
+          <CampoDeEntrada
+            type="text"
+            name="capa"
+            id="capa"
+            placeholder="http://..."
+          />
         </CampoDeFormulario>
         <CampoDeFormulario>
           <Label htmlFor="dataEvento">Data do evento</Label>
-          <CampoDeEntrada type="date" id="dataEvento" />
+          <CampoDeEntrada type="date" name="dataEvento" id="dataEvento" />
         </CampoDeFormulario>
         <CampoDeFormulario>
           <Label htmlFor="temaEvento">Tema do evento</Label>
